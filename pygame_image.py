75b06a1ddcd2c334b958a572ee572dc9e9252ccc
aoi <c0a2315791@edu.teu.ac.jp>
2024-11-19 17:16:24 +0900

    ugoku

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,32 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_flip = pg.transform.flip(bg_img, 1, 0)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, 1, 0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+    
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%800
+        x = tmr%3200
+        key_lst = pg.key.get_pressed()
         screen.blit(bg_img, [-x, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(bg_flip, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_flip, [-x+4800, 0])
+
+        screen.blit(kk_img, kk_rct)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1, 0)
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, 1)
         pg.display.update() 
         tmr += 1
         clock.tick(200)
